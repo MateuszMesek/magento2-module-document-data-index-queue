f17766b180541e53e5df24d0e1f3341d6b68c8ee
Mateusz Mesek <mateusz@mesek.pl>
2022-04-28 20:19:17 +0200

    wip

---
+++ src/Plugin/MergeConfig/OnPublisherXmlReader.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+/**
+ * TODO: fix on issue related to broken interceptor created by setup:di:compile
+ * @see https://github.com/magento/magento2/pull/35389
+ */
+
+namespace MateuszMesek\DocumentDataIndexQueue\Plugin\MergeConfig;
+
+class OnPublisherXmlReader extends OnReader
+{
+
+}
