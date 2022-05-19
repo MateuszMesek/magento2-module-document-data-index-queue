9b26f16df9f16fd1044834848f17cf42c262acb6
Mateusz Mesek <mateusz@mesek.pl>
2022-05-19 09:01:27 +0200

    added mview indexers support

--- src/DimensionalIndexer.php
+++ src/DimensionalIndexer.php
@@ -4,21 +4,30 @@ namespace MateuszMesek\DocumentDataIndexQueue;
 
 use Magento\Framework\Indexer\DimensionalIndexerInterface;
 use MateuszMesek\DocumentDataIndexQueueApi\PublisherInterface;
+use Psr\Log\LoggerInterface;
+use Throwable;
 use Traversable;
 
 class DimensionalIndexer implements DimensionalIndexerInterface
 {
+    private LoggerInterface $logger;
     private PublisherInterface $publisher;
 
     public function __construct(
+        LoggerInterface $logger,
         PublisherInterface $publisher
     )
     {
+        $this->logger = $logger;
         $this->publisher = $publisher;
     }
 
     public function executeByDimensions(array $dimensions, Traversable $entityIds)
     {
-        $this->publisher->publish($dimensions, $entityIds);
+        try {
+            $this->publisher->publish($dimensions, $entityIds);
+        } catch (Throwable $exception) {
+            $this->logger->emergency($exception);
+        }
     }
 }
